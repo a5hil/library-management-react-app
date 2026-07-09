766e773c10e8932894426ccdd3d0c3f29c764fd9
a5hil <ashil555anto@gmail.com>
2026-07-09 19:10:04 +0530

    SearchBook Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddBook from './Components/AddBook'
+import SearchBook from './Components/SearchBook'
+import DeleteBook from './Components/DeleteBook'
+import ViewBook from './Components/ViewBook'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
     </>
   )
 }
--- src/Components/AddBook.jsx
+++ src/Components/AddBook.jsx
@@ -1,65 +1,66 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const AddBook = () => {
   return (
     <div>
-        <NavBar />
-            <h2 className="text-center p-5">Add Book</h2>
-            <div className="container">
-                <div className="row">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <div className="row g-4">
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Book Code/ISBN</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Book Title</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Author</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Publisher</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Category/Genre</label>
-                                <select className="form-control">
-                                    <option>Fiction</option>
-                                    <option>Non-Fiction</option>
-                                    <option>Science</option>
-                                </select>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Edition</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Number of Copies</label>
-                                <input type="number" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Price</label>
-                                <input type="number" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Publication Year</label>
-                                <input type="number" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Shelf Location</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button className="btn btn-dark">Add Book</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      <NavBar />
+      <h2 className="text-center p-5">Add Book</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-4">
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Book Code/ISBN</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Book Title</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Author</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Publisher</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Category/Genre</label>
+                <select className="form-control">
+                  <option>Fiction</option>
+                  <option>Non-Fiction</option>
+                  <option>Science</option>
+                </select>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Edition</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Number of Copies</label>
+                <input type="number" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Price</label>
+                <input type="number" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Publication Year</label>
+                <input type="number" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Shelf Location</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-dark">Add Book</button>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
