bba0770539c1e8b1bbae0477ff0ea29ff4233213
a5hil <ashil555anto@gmail.com>
2026-07-10 11:33:47 +0530

    Dynamic product added

--- src/Components/ViewBook.jsx
+++ src/Components/ViewBook.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import NavBar from './NavBar'
+import { useState } from 'react'
 
 const ViewBook = () => {
+
+    const [data, showdata] = useState(
+        [
+            { "name": "White Nights", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/086/9780241252086.jpg", "author": "John Doe", "price": "$19.99"},
+            { "name": "The Alchemist", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/980/9788172234980.jpg", "author": "Paulo Coelho", "price": "$19.99"},
+            { "name": "The Road", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/792/9781035003792.jpg", "author": "John Doe", "price": "$79.99"}, 
+            { "name": "Project Hail Mary", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/468/9781529157468.jpg", "author": "Andy Weir", "price": "$39.99"},
+            { "name": "Harry Potter", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/652/9781408855652.jpg", "author": "J.K. Rowling", "price": "$99.99"},
+            { "name": "Fourth Wing", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/019/9780349437019.jpg", "author": "John Doe", "price": "$19.99"},
+            { "name": "Red Rising", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/993/9781444758993.jpg", "author": "Pierce Brown", "price": "$19.99"},
+            { "name": "Camille", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/128/9780857054128.jpg", "author": "Kevin Brown", "price": "$29.99"},
+            { "name": "Orbital", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/086/9780241252086.jpg", "author": "Samantha Harvey", "price": "$11.99"},
+            { "name": "11.22.63", "cover": "https://www.bookswagon.com/BW/productimages/mainimages/019/9780349437019.jpg", "author": "Stephen King", "price": "$49.99"}
+        ]
+    )
+
     return (
         <div>
             <NavBar />
@@ -12,217 +29,33 @@ const ViewBook = () => {
                         className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"
                     >
                         <div className="row g-4">
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/086/9780241252086.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">White Nights</h5>
-                                        <p className="card-text">
-                                            Author: John Doe
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$19.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/980/9788172234980.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">The Alchemist</h5>
-                                        <p className="card-text">
-                                            Author: Paulo Coelho
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$19.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/792/9781035003792.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">The Road</h5>
-                                        <p className="card-text">
-                                            Author: John Doe
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$79.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/468/9781529157468.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">Project Hail Mary</h5>
-                                        <p className="card-text">
-                                            Author: Andy Weir
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$39.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/652/9781408855652.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">Harry Potter</h5>
-                                        <p className="card-text">
-                                            Author: J.K. Rowling
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$99.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/019/9780349437019.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">Fourth Wing</h5>
-                                        <p className="card-text">
-                                            Author: John Doe
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$19.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/993/9781444758993.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">Red Rising</h5>
-                                        <p className="card-text">
-                                            Author: Pierce Brown
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$19.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/128/9780857054128.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">Camille</h5>
-                                        <p className="card-text">
-                                            Author: Kevin Brown
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$29.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/936/9781529922936.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">Orbital</h5>
-                                        <p className="card-text">
-                                            Author: Samantha Harvey
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$11.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
-                            >
-                                <div className="card">
-                                    <img
-                                        src="https://www.bookswagon.com/BW/productimages/mainimages/333/9781444727333.jpg"
-                                        className="card-img-top"
-                                        alt="..." height="300px"
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">11.22.63</h5>
-                                        <p className="card-text">
-                                            Author: Stephen King
-                                        </p>
-                                        <p className="card-text">
-                                            <strong>$49.99</strong>
-                                        </p>
-                                        <a href="#" className="btn btn-primary">Add to cart</a>
-                                    </div>
-                                </div>
-
-                            </div>
+                            {data.map(
+                                (value, index) => {
+                                    return (
+                                        <div
+                                            className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
+                                        >
+                                            <div className="card">
+                                                <img
+                                                    src={value.cover}
+                                                    className="card-img-top"
+                                                    alt="..." height="300px"
+                                                />
+                                                <div className="card-body">
+                                                    <h5 className="card-title">{value.name}</h5>
+                                                    <p className="card-text">
+                                                        Author: {value.author}
+                                                    </p>
+                                                    <p className="card-text">
+                                                        <strong>${value.price}</strong>
+                                                    </p>
+                                                    <a href="#" className="btn btn-primary">Add to cart</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            )}                            
                         </div>
                     </div>
                 </div>
